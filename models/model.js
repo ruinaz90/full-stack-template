51a772c6ab955e28dd23f2319448d5d5c97292c6
ruinaz90 <rzhao9002@gmail.com>
2022-08-17 20:06:41 -0400

    added models folder

---
+++ models/model.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose')
+
+/*const someSchema = new mongoose.Schema({
+  text: {
+    type: String,
+    required: true,
+  },
+  completed: {
+    type: Boolean,
+    required: true,
+  }
+})
+
+module.exports = mongoose.model('nameOfDB', someSchema)*/
